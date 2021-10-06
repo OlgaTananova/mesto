f7af11aeb684091adcc66e716279392cd8c8da79
Olga Tananova <tananovao@yandex.ru>
2021-10-06 14:11:55 +0500

    feature/load-initial-cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,32 @@
+/** Элементы (кнопки и блоки) **/
 // Выбираем блок профиля пользователя
-let profile = document.querySelector('.profile');
+const profile = document.querySelector('.profile');
 // Выбираем элемент с именем пользователя
-let profileName = profile.querySelector('.profile__name');
+const profileName = profile.querySelector('.profile__name');
 // Выбираем элемент с описанием пользователя
-let profileDescription = profile.querySelector('.profile__description');
+const profileDescription = profile.querySelector('.profile__description');
 //  Выбираем кнопку редактирования профиля
-let editButton = profile.querySelector('.profile__edit-button');
+const editButton = profile.querySelector('.profile__edit-button');
 // Выбираем блок со попапом редактирования профиля
-let popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup');
 // Выбираем кнопку закрытия попапа редактирования профиля
-let popupCloseButton = popup.querySelector('.popup__close-button');
+const popupCloseButton = popup.querySelector('.popup__close-button');
 // Выбираем форму редактирования профиля
-let formElement = popup.querySelector('.popup__form');
+const formElement = popup.querySelector('.popup__form');
 // Выбираем поле с именем пользователя
-let nameInput = formElement.querySelector('.popup__form-item_type_name');
+const nameInput = formElement.querySelector('.popup__form-item_type_name');
 // Выбираем поле с описанием пользователя
-let jobInput = formElement.querySelector('.popup__form-item_type_description');
+const jobInput = formElement.querySelector('.popup__form-item_type_description');
+// Выбираем блок вставки карточек
+const cardElementContainer = document.querySelector('.elements');
+// Массив карточек для загрузке на странице
+const initialCards = [
+  {name: 'Архыз', link:  '../images/element_photo_arhyz.jpg',},
+  {name: 'Челябинская область', link: '../images/element_photo_chelyabinsk-oblast.jpg'},
+  {name: 'Иваново', link: '../images/element_photo_ivanovo.jpg'},
+  {name: 'Камчатка', link: '../images/element_photo_kamchatka.jpg'},
+  {name: 'Холмогорский район', link: '../images/element_photo_kholmogorsky-rayon.jpg'},
+  {name: 'Байкал', link: '../images/element_photo_baikal_2.jpg'}];
 
 /* Функция открытия попапа редактирования профиля (автоматически вставляются
 значения из профайла */
@@ -37,6 +48,25 @@ function formSubmitHandler(evt) {
   profileName.textContent = nameInput.value;
   profileDescription.textContent = jobInput.value;
   closePopup()
+}
+
+/* Функция автоматической подгрузки карточек при открытии страницы */
+function loadInitialCards (array) {
+  array.forEach((item, index) => {
+    let cardName = array[index].name;
+    let cardImageLink = array[index].link;
+    addCardElement(cardName,cardImageLink);
+  });
+}
+loadInitialCards(initialCards);
+
+/* Функция создания карточки */
+function addCardElement (cardName, cardImageLink) {
+  const cardElementTemplate = document.querySelector('#element-template').content;
+  const cardElement = cardElementTemplate.querySelector('.element').cloneNode(true);
+  cardElement.querySelector('.element__caption').textContent = cardName;
+  cardElement.querySelector('.element__image').src = cardImageLink;
+  cardElementContainer.append(cardElement);
 }
 
 // Функции отслеживания поведения пользователя
